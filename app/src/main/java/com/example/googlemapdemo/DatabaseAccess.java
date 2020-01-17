cf834e8c32ea4932ba43dd5deb89f8daae44ee1c
Zocozoid <zoran-90@hotmail.com>
2020-01-17 10:18:46 +0100

    We commit the all activities of the project!!!

---
+++ app/src/main/java/com/example/googlemapdemo/DatabaseAccess.java
@@ -0,0 +1,54 @@
+package com.example.googlemapdemo;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseAccess {
+    private SQLiteOpenHelper openHelper;
+    private SQLiteDatabase db;
+    private static DatabaseAccess instance;
+    Cursor c = null;
+
+    // private constructor so that that object creation from outside the class is avoided
+    private DatabaseAccess(Context context){
+        this.openHelper=new DatabaseOpenHelper(context);
+
+    }
+
+    //to return the single instance of database
+
+    public static DatabaseAccess getInstance(Context context){
+        if (instance==null){
+            instance=new DatabaseAccess(context);
+        }
+        return instance;
+    }
+
+    // to open the database
+
+    public void open(){
+        this.db=openHelper.getWritableDatabase();
+    }
+
+    //closing the database connection
+
+    public void close(){
+        if (db!=null){
+            this.db.close();
+        }
+    }
+
+    // create a method to query and return the result from database
+
+    public String getLongitude (String name){
+        c=db.rawQuery("select Longitude from Table1 where Name = '"+name+"'", new String[]{});
+        StringBuffer buffer = new StringBuffer();
+        while (c.moveToNext()){
+            String longitude = c.getString(0);
+            buffer.append(""+longitude);
+        }
+        return buffer.toString();
+    }
+}
--- app/src/main/java/com/example/googlemapdemo/SecondView.java
+++ app/src/main/java/com/example/googlemapdemo/SecondView.java
@@ -4,9 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,27 +12,30 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+public class SecondView extends FragmentActivity implements OnMapReadyCallback {
 
 
     GoogleMap map;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
 
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
-        LatLng Skopje = new LatLng(41.99646, 21.43141);
-        map.addMarker(new MarkerOptions().position(Skopje).title("Skopje"));
-        map.moveCamera(CameraUpdateFactory.newLatLng(Skopje));
+        LatLng Macedonia = new LatLng(41.6086 , 21.7453 );
+        map.addMarker(new MarkerOptions().position(Macedonia).title("Macedonia"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(Macedonia));
+
     }
 }
