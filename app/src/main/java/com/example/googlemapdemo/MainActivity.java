c1fed3b7302fc644b4b40a987d4588fd201a5352
Zocozoid <zoran-90@hotmail.com>
2020-01-21 10:51:39 +0100

    Activity shifting resolved!!

--- app/src/main/java/com/example/googlemapdemo/MainActivity.java
+++ app/src/main/java/com/example/googlemapdemo/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.googlemapdemo;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,9 +17,11 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import com.example.googlemapdemo.SecondView;
+
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
-
+    Button button;
     GoogleMap map;
 
     @Override
@@ -25,10 +29,25 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button button = (Button) findViewById(R.id.listClients);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openSecondView();
+            }
+        });
+
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+    }
+
+    public void openSecondView(){
+        Intent intent = new Intent(MainActivity.this, SecondView.class);
+        startActivity(intent);
     }
 
     @Override
@@ -37,5 +56,8 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng Skopje = new LatLng(41.99646, 21.43141);
         map.addMarker(new MarkerOptions().position(Skopje).title("Skopje"));
         map.moveCamera(CameraUpdateFactory.newLatLng(Skopje));
+        map.setMinZoomPreference(10.0f);
     }
+
 }
+
--- app/src/main/java/com/example/googlemapdemo/SecondView.java
+++ app/src/main/java/com/example/googlemapdemo/SecondView.java
@@ -3,7 +3,10 @@ package com.example.googlemapdemo;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,28 +17,40 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class SecondView extends FragmentActivity implements OnMapReadyCallback {
 
-
     GoogleMap map;
-
-
+    Button btnClose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second_view);
+
+        Button btnClose = (Button) findViewById(R.id.btn_close);
+
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity();
+            }
+        });
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+    }
 
+    public void openMainActivity(){
+        Intent intent = new Intent(SecondView.this, MainActivity.class);
+        startActivity(intent);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
-        LatLng Macedonia = new LatLng(41.6086 , 21.7453 );
-        map.addMarker(new MarkerOptions().position(Macedonia).title("Macedonia"));
-        map.moveCamera(CameraUpdateFactory.newLatLng(Macedonia));
+        LatLng Skopje = new LatLng(41.99646 , 21.43141 );
+        map.addMarker(new MarkerOptions().position(Skopje).title("Skopje"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(Skopje));
+
 
     }
 }
